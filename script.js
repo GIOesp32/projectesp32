ce8f4b98b0495cc42b6dbf0946d296ed2148537e
GIOesp32 <tn.secure12anonimus@proton.me>
2026-05-20 10:25:48 +0200

    Update event listeners to use popup for status
    Replaced 'stato' element with 'popup' for status display.

--- script.js
+++ script.js
@@ -1,21 +1,19 @@
 let rosso = document.getElementById('b-rosso');
 let verde = document.getElementById('b-verde');
-let stato = document.getElementById('stato');
+let popup = document.getElementById('popup');
 
 
 rosso.addEventListener('click', () => {
     document.body.style.backgroundColor = 'red';
 
-
-    stato.textContent = "ACCESO";
-    stato.style.color = "red";
+    popup.textContent = "ACCESO";
+    popup.style.color = "red";
 });
 
 verde.addEventListener('click', () => {
     document.body.style.backgroundColor = 'green';
 
-
-    stato.textContent = "SPENTO";
-    stato.style.color = "green";
+    popup.textContent = "SPENTO";
+    popup.style.color = "green";
 });
 
